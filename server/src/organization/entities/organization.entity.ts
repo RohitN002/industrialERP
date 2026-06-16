f66a902e3582f60c71c0163a4d49eceee78e1e9a
RohitN002 <rohitrandy002@gmail.com>
2026-06-16 20:26:23 +0530

    org enitity

--- server/src/organization/entities/organization.entity.ts
+++ server/src/organization/entities/organization.entity.ts
@@ -1,1 +1,11 @@
-export class Organization {}
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+
+@Injectable()
+export class Organization {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async create(data) {
+    return this.prisma.organization.create({ data });
+  }
+}
