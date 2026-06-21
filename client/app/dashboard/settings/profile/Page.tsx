44fade34834aa38c7ee6a651b193e0870af7b54d
RohitN002 <rohitrandy002@gmail.com>
2026-06-21 15:16:02 +0530

    product page

--- client/app/dashboard/settings/profile/Page.tsx
+++ client/app/dashboard/settings/profile/Page.tsx
@@ -2,13 +2,26 @@
 
 import { useProfile } from "@/lib/store/useProfile";
 import ProfileForm from "@/modules/components/profile/ProfileForm";
+import {
+  EmptyState,
+  ErrorState,
+  LoadingState,
+} from "@/modules/components/shared";
 
 export default function ProfilePage() {
   const { data: organization, isLoading, isError } = useProfile();
   return (
     <div className="flex flex-col bg-background mt-10">
       <h3 className="text-2xl font-bold">Organization Profile</h3>
-      <ProfileForm />
+      {isLoading ? (
+        <LoadingState message="loading Profile details" />
+      ) : !organization?.length ? (
+        <EmptyState message="Error Loading Profile" />
+      ) : !isError ? (
+        <ErrorState message="Error Loading Profile" />
+      ) : (
+        <ProfileForm />
+      )}
     </div>
   );
 }
